66de7aee47d6b9a9a7cce8aecdd49080af932c0f
blt1305 <blt1305@gmail.com>
2023-06-18 11:44:55 +0300

    todo created via POST

--- tms/todos/views.py
+++ tms/todos/views.py
@@ -22,10 +22,19 @@ def todos(request):
     return render(request, 'todos.html', {'title': 'Список дел', 'todos': TODOS})
 
 def home_todos(request):
-    return JsonResponse({'todos': TODOS})
+    if request.method == 'GET':
+        return JsonResponse({'todos': TODOS})
+    elif request.method == 'POST':
+        todo = dict(request.POST)
+        todo['todo_id'] = int(todo['todo_id'][0])
+        todo['title'] = todo['title'][0]
+        todo['text'] = todo['text'][0]
+        TODOS.append(todo)
+        return JsonResponse({'todos': todo})
 
 def get_todo(request, todo_id):
     todo = next((todo for todo in TODOS if todo['todo_id'] == todo_id), None)
     if todo:
-        return JsonResponse({'todo': todo})
+        return JsonResponse({'todos': todo})
     return HttpResponse(status=404)
+
