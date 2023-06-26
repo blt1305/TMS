5a4109796641461dd632b1dc9472d02ecaaf8576
blt1305 <blt1305@gmail.com>
2023-06-26 15:12:19 +0300

    model change

--- tms/todos/models.py
+++ tms/todos/models.py
@@ -1,7 +1,12 @@
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
 
 class Todo(models.Model):
     name = models.CharField(max_length=120, default='')
-    description = models.TextField(default='')
-    created_date = models.DateField(auto_now_add=True)
-    author = models.CharField(max_length=50, default='')
+    description = models.TextField(default='', help_text='Напишите что-нибудь')
+    created_date = models.DateField(default=date.today)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
