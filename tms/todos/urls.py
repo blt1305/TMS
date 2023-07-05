ec4f0d0f73ae12023e4a66d897472ac99b4a4789
blt1305 <blt1305@gmail.com>
2023-07-05 16:53:05 +0300

    Добавлены страницы к приложению. Прописаны URL-адреса в шаблонах.

--- tms/todos/urls.py
+++ tms/todos/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.todos, name="todos"),
-    path('todo/', views.home_todos),
-    path('todo/<int:todo_id>/', views.get_todo),
-    path('addtask/', views.add_task)
+    path('todo/', views.home_todos, name = 'home_todos'),
+    path('todo/<int:todo_id>/', views.get_todo, name="todo"),
+    path('addtask/', views.add_task, name="add_task"),
+    path('about/', views.about, name = 'about'),
+    path('contact/', views.contact, name = 'contact'),
+    path('login/', views.login, name = 'login')
 ]
