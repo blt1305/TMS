181f28832d6ff2ef66af23ccbe050d18d84de129
blt1305 <blt1305@gmail.com>
2023-08-29 18:59:39 +0300

    работа сериализатора на примере тестового класса и тестовой ф-ции

--- tms/todos/serializers.py
+++ tms/todos/serializers.py
@@ -1,8 +1,25 @@
 from .models  import Todo
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
+class TodoModel:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
 
 
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['id', 'title', 'description', 'created_date', 'author']
+class TodoSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+
+def encode():
+    model = TodoModel('домашняя работа', 'уборка')
+    model_sr = TodoSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+
+# class Meta:
+#     model = Todo
+#     fields = ['id', 'title', 'description', 'created_date', 'author']
